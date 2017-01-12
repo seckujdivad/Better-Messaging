97f240ec3ed20384c7fc7cc61326f6f15ff64cd5
David Juckes <david.a.juckes@tesco.net>
2017-01-12 21:01:33 +0000

    Can connect to server and see a sidebar containing all available boards
    That is a huge understatement. The server is massively improved with
    more commands. Still need GUI for the client and then I should be done.
    
    Hopefully some anti-spam too.

--- client.py
+++ client.py
@@ -1,23 +1,23 @@
-global fonts, connection, root
+global fonts, connection, root, connect
 import tkinter as tk
 import socket, time
 
 servers = [['DAVID-JUCKES', 5000, 'For testing']]
 server = None
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect(host, port):
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect((host, port))
+    return connection
 
 def cmd_handle(listbox, choice):
     server = servers[choice]
-    connection.connect((server[0], server[1]))
-    connection.send(b'001')
     connection.recv(1024).decode()
-    
 
 for x in range(len(servers)):
     s = servers[x]
     try:
-        connection.connect((s[0], s[1]))
+        connection = connect(s[0], s[1])
         connection.send(b'000')
         time.sleep(0.5)
         servers[x][2] += ' - ' + connection.recv(1024).decode()
@@ -37,14 +37,25 @@ def pick_server(serverlist, selectorframe):
         pass
     else:
         print(choice)
+        server = servers[choice]
+        print(server)
+        connection = connect(server[0], server[1])
+        connection.send(b'001')
         selectorframe.destroy()
         frame = tk.Frame(root)
         scrollbar = tk.Scrollbar(frame)
-        msg_output = tk.Listbox(frame, height=30, width=150, yscrollcommand=scrollbar.set)
-        msg_output.pack(fill=tk.BOTH, side=tk.LEFT)
+        board_list = tk.Listbox(frame, height=30, width=30, yscrollcommand=scrollbar.set)
+        board_list.pack(fill=tk.BOTH, side=tk.LEFT)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        scrollbar.config(command=msg_output.yview)
+        scrollbar.config(command=board_list.yview)
         frame.pack(fill=tk.BOTH)
+        li = connection.recv(1024).decode()
+        if li.startswith('101'):
+            pli = eval(li[3:])
+            for item in pli:
+                board_list.insert(tk.END, item)
+        else:
+            raise Exception('Got message from server - ' + li)
 
 root = tk.Tk()
 root.title('Better Messaging')
--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def get_ip_ids():
         line = line.split(':')
         ipids[line[0]] = line[1]
 
-get_ip_ids()
+#get_ip_ids()
 
 server.listen(5)
 print('Server online')
@@ -37,34 +37,57 @@ def multi(server):
     def session(client, address):
         global sdata
         print('Connected to', address[0])
+        board = None
         while True:
             data = client.recv(1024).decode()
+            if data == None or data == '':
+                continue
             print(data)
             cmd = data[:3]
             try:
                 arg = data[3:]
             except IndexError:
                 arg = None
-            get_ip_ids()
+            #get_ip_ids()
             if cmd == '000':
                 if 'clist' in sdata.cfg:
                     client.send(sdata.cfg['clist'].encode())
                 else:
                     client.send(b"online")
-            elif cmd == '001' and not arg == None:
+            elif cmd == '001': #Show all boards
+                li = os.listdir('server/chats/')
+                client.send(bytes('101' + str(li), 'UTF-8'))
+            elif cmd == '002' and not arg == None: #Set board
                 if os.path.isdir('server/chats/' + arg):
-                    client.send('200')
+                    board = arg
+                else:
+                    client.send(b'201')
+            elif cmd == '003' and not arg == None: #Make new board
+                if os.path.isdir('server/chats/' + arg):
+                    client.send(b'200')
                 else:
                     os.mkdir('server/chats/' + arg)
                     file = open('server/chats/' + arg + '/history.txt', 'w')
                     if address[0] in ipids:
-                        file.write('Room made by ' + ipids[address[0]])
+                        file.write('Board made by ' + ipids[address[0]])
                     else:
-                        file.write('Room made by ' + address[0])
+                        file.write('Board made by ' + address[0])
                     file.close()
                     file = open('server/chats/' + arg + '/people.txt', 'w')
                     file.write('["' + address[0] + '"]')
                     file.close()
+            elif cmd == '004' and not board == None and os.path.isdir('server/chats/' + arg):
+                file = open('server/chats/' + board + '/history.txt', 'r')
+                fc = file.readlines()
+                file.close()
+                client.send(b'100')
+                for line in fc:
+                    client.send(bytes('103' + line, 'UTF-8'))
+                client.send(b'104')
+            elif cmd == '005' and not board == None and not arg == None:
+                file = open('server/chats/' + board + '/history.txt', 'a')
+                file.write('\n' + arg)
+                file.close()
     while True:
         client, address = server.accept()
         sdata.clients.append(address)
